57f674a45b3aa97dc47557b9c9dfa6f983c5b066
Steven <peragwin@gmail.com>
2018-02-20 21:19:21 -0800

    add LogScale; clean up bucketer and provision edge case where the specified number of buckets would cause consecutive indices to be equal resulting in empty buckets

--- audio/util/bucketer.go
+++ audio/util/bucketer.go
@@ -5,6 +5,7 @@ import (
 	"math"
 )
 
+// Scale represents the scale that is used to calculate bucket indices.
 type Scale interface {
 	To(float64) float64
 	From(float64) float64
@@ -12,6 +13,7 @@ type Scale interface {
 
 type melScale struct{}
 
+// MelScale is a scale defined by how humans perceive pitch differences.
 var MelScale *melScale
 
 func (s *melScale) To(val float64) float64 {
@@ -20,6 +22,18 @@ func (s *melScale) To(val float64) float64 {
 
 func (s *melScale) From(val float64) float64 {
 	return 700 * (math.Exp(val/1127.0) - 1)
+}
+
+type logScale struct{}
+
+// LogScale is a typical log2 scale.
+var LogScale *logScale
+
+func (s *logScale) To(val float64) float64 {
+	return math.Log2(1 + val)
+}
+func (s *logScale) From(val float64) float64 {
+	return math.Exp2(val) - 1
 }
 
 // Bucketer puts the specturn into N buckets using a mel frequency scale
@@ -32,16 +46,40 @@ type Bucketer struct {
 	indices []int
 }
 
+// NewBucketer creates a new Bucketer for a frame of @frameSize based on @scale and N @buckets,
+// starting with [0:@fMin] and going to [@fMax:<nyquist>].
 func NewBucketer(scale Scale, buckets, frameSize int, fMin, fMax float64) *Bucketer {
 	sMin := scale.To(fMin)
 	sMax := scale.To(fMax)
 	space := (sMax - sMin) / float64(buckets)
 	indices := make([]int, buckets-1)
+	lastIdx := 0
+	offset := 1
+	// in scale space, how far is a unit index
+	offsetDelta := (sMax - sMin) / float64(frameSize)
+
 	for i := range indices {
-		idx := scale.From(float64(i+1) * space)
-		indices[i] = int(math.Ceil(
-			float64(frameSize) * idx / fMax))
+		// the bucket spacing needs to adjust if we've accumulated offset
+		adjSpace := space - offsetDelta*float64(offset)/float64(buckets)
+
+		v := scale.From(float64(i+1)*(adjSpace) + sMin + offsetDelta*float64(offset))
+		idx := int(math.Ceil(
+			float64(frameSize) * v / fMax))
+
+		// this is a special edge case when requested number of buckets is too high
+		// and we'd otherwise have two sequential index values that round to the same number.
+		if idx <= lastIdx {
+			idx = lastIdx + 1
+			offset++
+		}
+		if idx >= frameSize {
+			idx = frameSize - 1
+		}
+
+		indices[i] = idx
+		lastIdx = idx
 	}
+
 	return &Bucketer{
 		Buckets: buckets,
 		Size:    frameSize,
@@ -50,6 +88,8 @@ func NewBucketer(scale Scale, buckets, frameSize int, fMin, fMax float64) *Bucke
 	}
 }
 
+// Bucket applys b.Buckets rectangular windows on the incoming frame and returns the sum in
+// each window in a len==b.Buckets []float64.
 func (b *Bucketer) Bucket(frame []float64) []float64 {
 	buckets := make([]float64, b.Buckets)
 	if len(frame) != b.Size {
@@ -76,14 +116,17 @@ func (b *Bucketer) Bucket(frame []float64) []float64 {
 	return buckets
 }
 
+// BucketProcessor is an asynchronous processor that puts incoming frames into buckets.
 type BucketProcessor struct {
 	Bucketer *Bucketer
 }
 
+// NewBucketProcessor creates a new bucket processor using a Bucketer.
 func NewBucketProcessor(b *Bucketer) *BucketProcessor {
 	return &BucketProcessor{b}
 }
 
+// Process kicks off a goroutine to process incoming @in frames and returns the output channel.
 func (b *BucketProcessor) Process(done chan struct{}, in chan []float64) chan []float64 {
 	out := make(chan []float64)
 
--- audio/util/bucketer_test.go
+++ audio/util/bucketer_test.go
@@ -4,15 +4,20 @@ import "testing"
 
 func TestBucketer(t *testing.T) {
 	size := 512
-
-	b := NewBucketer(MelScale, 16, size, 32, 16000)
-	t.Log(b.indices)
-
 	frame := make([]float64, size)
 	for i := range frame {
 		frame[i] = 1.0
 	}
 
+	// test and print MelScale
+	b := NewBucketer(MelScale, 64, size, 32, 16000)
+	t.Log(b.indices)
 	buckets := b.Bucket(frame)
+	t.Log(buckets, len(buckets))
+
+	// test and print LogScale
+	b = NewBucketer(LogScale, 60, size, 32, 16000)
+	t.Log(b.indices)
+	buckets = b.Bucket(frame)
 	t.Log(buckets, len(buckets))
 }
